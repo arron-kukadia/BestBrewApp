f6372da3e3a55a8c5e1d924bb0f1d81a530d4fe8
arron <arron.kukadia@olioex.com>
2026-01-14 09:55:11 +0000

    amend theme

--- src/theme/spacing.ts
+++ src/theme/spacing.ts
@@ -8,11 +8,11 @@ export const spacing = {
 } as const;
 
 export const borderRadius = {
-  xs: 4,
-  sm: 8,
-  md: 12,
-  lg: 16,
-  xl: 24,
+  xs: 8,
+  sm: 12,
+  md: 16,
+  lg: 24,
+  xl: 32,
   full: 9999,
 } as const;
 
